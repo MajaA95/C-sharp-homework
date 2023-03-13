ce56a1193f7e6d5079d10880feb4a5313b90cb92
MajaA95 <apostolovamaja95@gmail.com>
2023-03-13 22:05:41 +0100

    homework 2

---
+++ homework2SwapNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿int firstNumber;
+int secondNumber;
+int temp;
+
+
+Console.WriteLine("Enter first number");
+firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter second number");
+secondNumber = Convert.ToInt32(Console.ReadLine());
+
+temp = firstNumber;
+firstNumber = secondNumber;
+secondNumber = temp;
+
+Console.WriteLine("After swap: ");
+Console.WriteLine("First number:" + firstNumber);
+Console.WriteLine("Second number:" + secondNumber);
+Console.Read();
