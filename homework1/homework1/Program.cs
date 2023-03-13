868a0f81e7c95759bff23cbe6e5f6202a212fc5e
MajaA95 <apostolovamaja95@gmail.com>
2023-03-13 19:14:45 +0100

    Add project files.

---
+++ homework1/homework1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Data.Common;
+
+double firstNumber;
+double secondNumber;
+double result;
+char operation;
+
+Console.WriteLine("Enter first number");
+firstNumber = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter second number");
+secondNumber = double.Parse(Console.ReadLine());   
+
+Console.WriteLine("Enter the operator");
+operation = char.Parse(Console.ReadLine());
+
+
+if(operation == '+')
+{
+    result = firstNumber + secondNumber;
+    Console.WriteLine(result);
+}
+if(operation == '-')
+{
+    result = firstNumber - secondNumber;
+    Console.WriteLine(result);
+}
+if(operation == '/')
+{
+    result = firstNumber / secondNumber;
+    Console.WriteLine(result);
+}
+if(operation == '*')
+{
+    result = firstNumber * secondNumber;
+    Console.WriteLine(result);
+}
+
+
+                    
---
+++ homework1/homework1Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿double firstNumber;
+double secondNumber;
+double thirdNumber;
+double fourthNumber;
+double result;
+
+
+
+Console.WriteLine("Enter the First number");
+firstNumber = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter the Second number");
+secondNumber = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter the Third number");
+thirdNumber = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter the Fourth number");
+fourthNumber = Convert.ToDouble(Console.ReadLine());
+
+result = (firstNumber + secondNumber + thirdNumber + fourthNumber) / 4;
+Console.WriteLine("The average of numbers is:" + result);
+
+
