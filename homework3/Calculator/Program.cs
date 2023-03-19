affdfd586489d0dbfd9db6828c5faa81021772e0
MajaA95 <apostolovamaja95@gmail.com>
2023-03-19 19:44:15 +0100

    homework3

---
+++ homework3/Calculator/Program.cs
@@ -0,0 +1,65 @@
+﻿
+int Main ()
+{
+   
+    Console.WriteLine("Enter operator");
+    char op = Convert.ToChar(Console.ReadLine());
+  
+    Console.WriteLine("Enter number 1");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Enter number 2");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+
+    int result = 0;
+
+    switch (op)
+        
+    {
+        
+        case '+':
+            result = Sum(number1, number2);
+            break;
+
+        case '-':
+            result = Substract(number1, number2);
+            break;
+
+        case '*':
+            result = Multiply(number1, number2);
+            break;
+
+        case '/':
+            result = Divide(number1, number2);
+            break;
+
+        default:
+            Console.WriteLine("You entered invalid operator");
+            break;
+
+    }
+    return result;
+  
+}
+
+int Sum(int num1, int num2)
+{
+    return num1 + num2;
+}
+int Substract(int num1, int num2)
+{
+    return num1 - num2;
+}
+int Multiply(int num1, int num2)
+{
+    return num1 * num2;
+}
+int Divide(int num1, int num2)
+{
+    return num1 / num2;
+}
+
+
+int res = Main();
+Console.WriteLine("The result is: " + res);
+
